e940a069f3939c97af36588a0d59ad1aed0bb05e
LukaszLebioda <lukasz.leb@gmail.com>
2022-11-25 15:40:05 +0100

    npm install, uninstall globally locally practice

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
-<<<<<<< HEAD
+// // SYNTAX:
 
-  
-=======
-// const express = require("express");
-// const app = express();
+const http = require("http"); // require - includes a core module we need (in this case http to make Node.js act as an HTTP server)
 
-// app.listen(3000)
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8081);
 
-console.log("Hello");
->>>>>>> 6b2ba14296196cf0bdc9ff34bc15e43b4e339611
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
+
+// ---------------------------------------------------------
+
+// console:
+// wyświetla w konsoli informację:
+// console.log("Some dummy content of mine"); 
+
+// // wyświetla w konsoli błąd:
+// console.error(new Error("Critical error, the computer will shut down"));  
+
+// // wyświetla w konsoli ostrzeżenie:
+// const name = "Łukasz";  
+// const age = 40;
+// console.warn(`Your name is ${name} and You're ${age} years old`); 
